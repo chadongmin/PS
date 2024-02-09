8fc41be0e86ef23430b7600892f057250ab74e9f
chadongmin <40655807+chadongmin@users.noreply.github.com>
2024-02-09 16:19:03 +0900

    [Bronze I] Title: 애너그램, Time: 148 ms, Memory: 16108 KB -BaekjoonHub

--- 백준/Bronze/6996. 애너그램/애너그램.java
+++ 백준/Bronze/6996. 애너그램/애너그램.java
@@ -1,19 +1,13 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
-import javax.swing.plaf.IconUIResource;
 
 public class Main {
 
     public static void main(String[] args)throws IOException {
-        int tmp1=0;
-        int tmp2=0;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Integer tc = Integer.parseInt(br.readLine());
@@ -22,28 +16,23 @@ public class Main {
             String str = br.readLine();
             String[] split = str.split(" ");
 
-            List list = new ArrayList();
-            char[] charArray = split[0].toCharArray();
-            for (var c : charArray){
-                list.add(c);
-            }
-            Collections.sort(list);
-
-            List list1 = new ArrayList();
-            char[] charArray1 = split[1].toCharArray();
-            for (var c : charArray1){
-                list1.add(c);
-            }
-            Collections.sort(list1);
+            List list = convertStringToList(split[0]);
+            List list1 = convertStringToList(split[1]);
 
             if (list.equals(list1)){
                 System.out.println(split[0] +" & "+split[1]+ " are anagrams.");
             }else {
                 System.out.println(split[0] +" & "+split[1]+ " are NOT anagrams.");
             }
-
         }
     }
-
-
+    private static List convertStringToList(String str) {
+        List list = new ArrayList();
+        char[] charArray = str.toCharArray();
+        for (var c : charArray){
+            list.add(c);
+        }
+        Collections.sort(list);
+        return list;
+    }
 }
