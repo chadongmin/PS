1e8c114d431963dd34e0011ba72847781adb96f1
chadongmin <40655807+chadongmin@users.noreply.github.com>
2024-06-27 16:15:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2468 ms, Memory: 186604 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,39 +1,52 @@
+
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-        int tc = sc.nextInt();
-        List<List<Integer>> list = new ArrayList<>();
+		int tc = sc.nextInt();
+		List<xy> list = new ArrayList<>();
 
-        for (int i = 0; i < tc; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            List<Integer> point = new ArrayList<>();
-            point.add(x);
-            point.add(y);
-            list.add(point);
-        }
+		for (int i = 0; i < tc; i++) {
 
-        Collections.sort(list, new Comparator<List<Integer>>() {
-            @Override
-            public int compare(List<Integer> o1, List<Integer> o2) {
-                if (o1.get(0).equals(o2.get(0))) {
-                    return o1.get(1) - o2.get(1);
-                }
-                return o1.get(0) - o2.get(0);
-            }
-        });
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			list.add(new xy(x, y));
+		}
 
-        for (List<Integer> l : list) {
-            System.out.println(l.get(0) + " " + l.get(1));
-        }
-    }
+		list.sort((o1, o2) ->
+		{
+			if (o1.x == o2.x) {
+				return o1.y - o2.y;
+			}
+			return o1.x - o2.x;
+		});
+
+		list.stream().forEach(ob -> System.out.println(ob.x + " " + ob.y));
+	}
+
+	static class xy {
+
+		int x;
+		int y;
+
+		public xy(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public int getY() {
+			return y;
+		}
+	}
+
 }
