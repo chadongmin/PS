fb504b3b7cf13bab105bbb406db295b65bf8f4f7
차동민 <40655807+chadongmin@users.noreply.github.com>
2024-07-23 09:33:17 +0900

    [Silver IV] Title: 문자열 집합, Time: 1076 ms, Memory: 68796 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,33 +1,54 @@
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
+
+	public static int cnt = 0;
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
 
-		String[] str = new String[N];
-		for (int i = 0; i < N; i++) {
-			str[i] = sc.next();
+		int tc = sc.nextInt();
+		int input = sc.nextInt();
+
+		List<String> targetList = new ArrayList<>();
+
+		for (int i = 0; i < tc; i++) {
+			targetList.add(sc.next());
 		}
-		Arrays.sort(str);
+		Collections.sort(targetList);
 
-		String[] target = new String[M];
-		int ans= 0;
-		for (int i = 0; i < M; i++) {
-			target[i] = sc.next();
-			if (Arrays.binarySearch(str, target[i]) >= 0){
-				ans++;
+		for (int i = 0; i < input; i++) {
+			String str = sc.next();
+			if (isExist(targetList, str)) {
+				cnt++;
 			}
 		}
-		System.out.println(ans);
+		System.out.println(cnt);
+
+	}
+
+	static boolean isExist(List<String> list, String str) {
+		int l = 0;
+		int r = list.size() - 1;
+
+		while (l <= r) {
+			int m = (l + r) / 2;
+			int compareResult = list.get(m).compareTo(str);
+
+			if (compareResult < 0) {
+				l = m + 1;
+			} else if (compareResult > 0) {
+				r = m - 1;
+			} else {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
