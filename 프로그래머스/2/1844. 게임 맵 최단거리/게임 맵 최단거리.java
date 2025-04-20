f138252c5795071cc32183afbd0f154e4db4d3e3
차동민 <40655807+chadongmin@users.noreply.github.com>
2025-04-20 20:43:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.22 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,33 +1,42 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
-        int[] dr = {-1, 0, 1, 0};
-		int[] dc = {0, 1, 0, -1};
-		Queue<int[]> Q = new LinkedList<>();
-        int n = maps.length;
-       
-        int m = maps[0].length;
         
-		Q.offer(new int[]{0, 0});
-		int L = 1;
-		while(!Q.isEmpty()){
-			int len = Q.size();
-			for(int i = 0; i < len; i++){
-				int[] p = Q.poll();
-                
-				for(int k = 0; k < 4; k++){
-					int nr = p[0] + dr[k];
-					int nc = p[1] + dc[k];
-				    if(nr == n-1 && nc == m-1) return L+1;
-                    if(nr >=0  && nr < n && nc >= 0 && nc < m && maps[nr][nc] == 1){
-                        maps[nr][nc] = 0;
-                        Q.offer(new int[]{nr, nc});    
-                    }
-                    
-				}
-			}
-			L++;
-		}
-		return -1;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        int[][] result;
+
+        Queue<int[]> queue = new LinkedList<>();
+        int N = maps.length;
+        int M = maps[0].length;
+
+        result = new int[N][M];
+        //1. 시작 노드 큐에 넣기
+        queue.add(new int[]{0, 0});
+        // 방문처리
+        result[0][0] = 1;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+
+            //갈 수 있는지 판단
+            for (int i = 0; i < 4; i++) {
+                int x = now[0] + dx[i];
+                int y = now[1] + dy[i];
+
+                if (x < 0 || y < 0 || x >= N || y >= M) {
+                    continue;
+                }
+                if (maps[x][y] == 0) {
+                    continue;
+                }
+                if (result[x][y] == 0) {
+                    queue.add(new int[]{x, y});
+                    result[x][y] = result[now[0]][now[1]] + 1;
+                }
+            }
+        }
+
+        return result[N - 1][M - 1] == 0 ? -1 : result[N - 1][M - 1];
     }
 }
