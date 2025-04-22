d89f14103ee3b4c6cf3dc0874692ca69a148e6b5
차동민 <40655807+chadongmin@users.noreply.github.com>
2025-04-22 19:27:06 +0900

    [level 2] Title: 괄호 회전하기, Time: 112.98 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,35 +1,39 @@
 import java.util.*;
 class Solution {
        public static int solution(String s) {
-int answer = 0;
 
-        A:
+        boolean isValid;
+        int answer = 0;
+
         for (int i = 0; i < s.length(); i++) {
-            Stack<Character> stack = new Stack<>();
-            String val = s.substring(i) + s.substring(0, i);
-
-            for (int j = 0; j < val.length(); j++) {
-                char c = val.charAt(j);
-                if (c == '(' || c == '{' || c == '[') {
-                    stack.push(c);
+            isValid = true;
+            String str = s.substring(i) + s.substring(0, i);
+            Stack<String> stack = new Stack<>();
+            for (int j = 0; j < s.length(); j++) {
+                String val = str.substring(j, j + 1);
+                if ((val.equals("(") || val.equals("[") || val.equals("{"))) {
+                    stack.push(val);
                 } else {
                     if (stack.isEmpty()) {
-                        continue A;
-                    }
-                    Character peek = stack.peek();
-                    if ((c == ')' && peek == '(') ||
-                            (c == ']' && peek == '[') ||
-                            (c == '}' && peek == '{')) {
-                        stack.pop();
+                        isValid = false;
                     } else {
-                        continue A;
+                        String top = stack.peek();
+                        if (val.equals(")") && top.equals("(") ||
+                                val.equals("}") && top.equals("{") ||
+                                val.equals("]") && top.equals("[")) {
+                            stack.pop();
+                        } else {
+                            isValid = false;
+                            break;
+                        }
                     }
                 }
             }
-            if (stack.isEmpty()) {
+            if (isValid && stack.isEmpty()) {
                 answer++;
             }
         }
         return answer;
-       }
+
+    }
 }
