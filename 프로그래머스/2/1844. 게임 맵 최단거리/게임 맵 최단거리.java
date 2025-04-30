33874be0ee1f9c5f6e9e375ae62f16c5f311ceca
차동민 <40655807+chadongmin@users.noreply.github.com>
2025-05-01 01:37:32 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.68 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,37 +1,42 @@
 import java.util.*;
 class Solution {
-    public static int solution(int[][] maps) {
+       public static int solution(int[][] maps) {
 
-        int[] dx = {1, 0, -1, 0};
-        int[] dy = {0, 1, 0, -1};
+        int row = maps.length; //세로길이
+        int col = maps[0].length; //가로 길이
 
-        int row = maps.length;
-        int column = maps[0].length;
-        int[][] result = new int[row][column];
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0, -1, 0, 1};
+
+        int[][] result = new int[row][col];
 
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0, 0});
         result[0][0] = 1;
+
         while (!q.isEmpty()) {
             int[] now = q.poll();
             for (int i = 0; i < 4; i++) {
-                int x = dx[i] + now[0];
-                int y = dy[i] + now[1];
+                int c = now[1] + dx[i];
+                int r = now[0] + dy[i];
 
-                if (x >= row || x < 0 || y >= column || y < 0) {
+
+                if (c >= col || c < 0 || r >= row || r < 0) {
                     continue;
                 }
-                if (maps[x][y] == 0) {
+                if (maps[r][c] == 0) {
                     continue;
                 }
-                if (result[x][y] == 0) {
-                    q.add(new int[]{x, y});
-                    result[x][y] = result[now[0]][now[1]] + 1;
+                if (result[r][c] == 0) {
+                    q.add(new int[]{r, c});
+                    result[r][c] = result[now[0]][now[1]] + 1;
                 }
             }
-
         }
-        if (result[row - 1][column - 1] == 0) return -1;
-        return result[row - 1][column - 1];
+
+        if (result[row - 1][col - 1] == 0) {
+            return -1;
+        }
+        return result[row - 1][col - 1];
     }
 }
